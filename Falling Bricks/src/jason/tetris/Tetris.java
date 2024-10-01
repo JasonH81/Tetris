1b1e9db7c40e118fadb4dd9d8a3d2b4ea6da26c4
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2024-09-30 20:46:02 -0400

    Added new classes for bricks

--- Falling Bricks/src/jason/tetris/Tetris.java
+++ Falling Bricks/src/jason/tetris/Tetris.java
@@ -1,13 +1,63 @@
 package jason.tetris;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import jason.mycomponents.ScorePanel;
+import jason.mycomponents.TitleLabel;
 
 public class Tetris extends JFrame {
 	private static final long serialVersionUID = 1L;
+	
+	private ScorePanel scorePanel = new ScorePanel(0, Color.CYAN);
+	private BricksPanel bricksPanel = new BricksPanel();
+	
+	public Tetris() {
+		initGUI();
+		
+		setTitle("Tetris");
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void initGUI() {
+		TitleLabel titleLabel = new TitleLabel("Tetris");
+		add(titleLabel, BorderLayout.PAGE_START);
+		
+		// main panel
+		JPanel mainPanel = new JPanel();
+		mainPanel.setBackground(Color.CYAN);
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		add(mainPanel, BorderLayout.CENTER);
+		
+		// score panel
+		mainPanel.add(scorePanel);
+		
+		// bricks panel
+		mainPanel.add(bricksPanel);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			String classname = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(classname);
+		}
+		catch (Exception e) {}
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Tetris();
+			}
+		});
 	}
 
 }
